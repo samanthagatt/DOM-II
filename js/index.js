354afd6e83767995b73746e7b5504eb100677e33
Samantha Gatt <smfgatt@gmail.com>
2020-02-24 17:29:50 -0500

    Adds a drag and drop event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+let dragEl;
+let dragTarg;
+
+const handleDragStart = (e) => {
+    e.dataTransfer.effectAllowed = 'move'
+    e.dataTransfer.dropEffect = 'move'
+    dragEl = e.currentTarget;
+};
+
+const handleDragEnter = (e) => {
+    dragTarg = e.currentTarget;
+};
+
+const handleDragEnd = (e) => {
+    dragTarg.insertAdjacentElement('afterend', dragEl);
+};
+
+const contentPick = document.getElementsByClassName('content-pick')[0];
+const pickChildren = Array.from(contentPick.children);
+pickChildren.forEach((child, i) => {
+    if (i == 1) {
+        child.children[1].textContent = 'Esse nobis incidunt tenetur deleniti sit cupiditate accusamus ad voluptatum maiores!';
+    }
+    if (i == 2) {
+        child.children[1].textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet odio autem iusto odit nam recusandae soluta accusamus consectetur.'
+    }
+    child.setAttribute('id', `${i}`);
+    child.setAttribute('draggable', true);
+    child.addEventListener('dragstart', handleDragStart);
+    child.addEventListener('dragenter', handleDragEnter);
+    child.addEventListener('dragend', handleDragEnd);
+});
